b42961879994a3dfda5c4000607bc353b9cfe5d4
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-03-04 23:34:47 +0100

    add nearest_neighbors function to find similar words

--- utils.py
+++ utils.py
@@ -9,3 +9,16 @@ def load_tokens(file_path):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def nearest_neighbors(word, vocab, w2v, top_n):
+    idx = vocab.word_to_idx[word]
+    vec = w2v.W_in[idx]
+
+    diffs = w2v.W_in - vec
+    dists = np.sqrt(np.sum(diffs ** 2, axis=1))
+
+    top = np.argsort(dists)[1 : top_n + 1]
+    print(f"Word: {word}")
+    for i in top:
+        print(f"- {vocab.idx_to_word[i]}")
