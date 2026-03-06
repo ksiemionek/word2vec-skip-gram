9b0ed0c7c4642fb0ad17ce6f8925d670d624d1dc
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-03-06 16:57:47 +0100

    update model file paths

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def main():
     # vocab = Vocabulary(FILE_PATH, MIN_COUNT)
     # w2v = Word2Vec(len(vocab), LEARNING_RATE, N_EMBEDDING, vocab.words_vectorized)
     # train_model(vocab, w2v, EPOCHS, WINDOW_SIZE, N_NEGATIVES)
-    # w2v.save_model("W_in", "W_out")
+    # w2v.save_model("model/W_in", "model/W_out")
 
     # LOAD THE TRAINED MODEL
     vocab = Vocabulary(FILE_PATH, MIN_COUNT)
     w2v = Word2Vec(len(vocab), LEARNING_RATE, N_EMBEDDING, vocab.words_vectorized)
-    w2v.load_model("W_in.npy", "W_out.npy")
+    w2v.load_model("model/W_in.npy", "model/W_out.npy")
 
     nearest_neighbors("jet", vocab, w2v, 5)
-    nearest_neighbors("brain", vocab, w2v, 5)
+    nearest_neighbors("brains", vocab, w2v, 5)
     nearest_neighbors("king", vocab, w2v, 5)
     nearest_neighbors("queen", vocab, w2v, 5)
     nearest_neighbors("apple", vocab, w2v, 5)
